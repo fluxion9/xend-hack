6aee1074e05c54cc2aa712253f1bacf97936a2a2
Isaac J.Newton <isaacjavannewton@gmail.com>
2023-11-18 18:01:28 +0100

    Modified some APIs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, make_response, render_template, request, redirect, url_for, jsonify
+from flask import Flask, make_response, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from json import dumps
 
 Tariff = 100
 
@@ -51,33 +52,82 @@ def index():
     
 @app.route('/api/create-meter/', methods=['POST', 'GET'])
 def create_meter():
-    mid = request.args.get('mid')
+    if request.method == 'GET':
+        mid = request.args.get('mid')
+    elif request.method == 'POST':
+        try:
+            json_data = request.get_json()
+            if json_data:
+                mid = json_data.get('mid')
+            else:
+                return make_response('', 422)
+        except Exception:
+            return make_response('', 422)
     if Registry.query.get(mid):
-        res = {"error": "mid taken"}
-        return jsonify(res), 403
+        response = {"error": "mid taken"}
+        response = dumps(response)
+        response = make_response(response, 403)
+        return response
     else:
-        fname = request.args.get('fname')
-        lname = request.args.get('lname')
-        usrname = request.args.get('usrname')
-        addr = request.args.get('addr')
+        if request.method == 'GET':
+            fname = request.args.get('fname')
+            lname = request.args.get('lname')
+            usrname = request.args.get('usrname')
+            addr = request.args.get('addr')
+        elif request.method == 'POST':
+            try:
+                json_data = request.get_json()
+                if json_data:
+                    fname = json_data.get('fname')
+                    lname = json_data.get('lname')
+                    usrname = json_data.get('usrname')
+                    addr = json_data.get('addr')
+                else:
+                    return make_response('', 422)
+            except Exception:
+                return make_response('', 422)
         new_registry = Registry(mid=mid, name=fname, surname=lname, address=addr, username=usrname)
         db1.session.add(new_registry)
         db1.session.commit()
         new_state = States(mid=mid, is_active=True, is_on=True, interval=15, balance=1000.0)
         db0.session.add(new_state)
         db0.session.commit()
-        res = {"success": "meter created"}
-        return jsonify(res), 200
+        response = {"success": "meter created"}
+        response = dumps(response)
+        response = make_response(response, 200)
+        return response
 
 @app.route('/api/feedback/', methods=['GET', 'POST'])
 def feedback():
-    mid = request.args.get('mid')
+    if request.method == 'GET':
+        mid = request.args.get('mid')
+    elif request.method == 'POST':
+        try:
+            json_data = request.get_json()
+            if json_data:
+                mid = json_data.get('mid')
+            else:
+                return make_response('', 422)
+        except Exception:
+            return make_response('', 422)
     if not Registry.query.get(mid):
-        jsonify([0, 0, 15, 0]), 200
+        return dumps([0, 0, 15, 0]), 200
     else:
-        voltage = request.args.get('vol')
-        current = request.args.get('amp')
-        energy = request.args.get('kwh')
+        if request.method == 'GET':
+            voltage = request.args.get('vol')
+            current = request.args.get('amp')
+            energy = request.args.get('kwh')
+        elif request.method == 'POST':
+            try:
+                json_data = request.get_json()
+                if json_data:
+                    voltage = json_data.get('usage')['vol']
+                    current = json_data.get('usage')['amp']
+                    energy = json_data.get('usage')['kwh']
+                else:
+                    return make_response('', 422)
+            except Exception:
+                return make_response('', 422)
         new_records = Records(mid=mid, voltage=voltage, current=current, energy=energy)
         db.session.add(new_records)
         db.session.commit()
@@ -92,14 +142,24 @@ def feedback():
             return str([0, 0, 15, 0])
         states = States.query.get(mid)
         if states:
-            res = [int(states.is_active), int(states.is_on), states.interval, states.balance]
-            return jsonify(res), 200
+            res = [int(states.is_active), int(states.is_on), states.interval, round(states.balance, 2)]
+            return dumps(res), 200
         else:
-            return jsonify([0, 0, 15, 0]), 200
+            return dumps([0, 0, 15, 0]), 200
 
 @app.route('/api/activate-meter/', methods=['GET', 'POST'])
 def activate_meter():
-    mid = request.args.get('mid')
+    if request.method == 'GET':
+        mid = request.args.get('mid')
+    elif request.method == 'POST':
+        try:
+            json_data = request.get_json()
+            if json_data:
+                mid = json_data.get('mid')
+            else:
+                return make_response('', 422)
+        except Exception:
+            return make_response('', 422)
     if not Registry.query.get(mid):
         return make_response('', 403)
     else:
@@ -111,7 +171,17 @@ def activate_meter():
 
 @app.route('/api/deactivate-meter/', methods=['GET', 'POST'])
 def deactivate_meter():
-    mid = request.args.get('mid')
+    if request.method == 'GET':
+        mid = request.args.get('mid')
+    elif request.method == 'POST':
+        try:
+            json_data = request.get_json()
+            if json_data:
+                mid = json_data.get('mid')
+            else:
+                return make_response('', 422)
+        except Exception:
+            return make_response('', 422)
     if not Registry.query.get(mid):
         return make_response('', 403)
     else:
@@ -123,7 +193,17 @@ def deactivate_meter():
     
 @app.route('/api/topup-meter/', methods=['GET', 'POST'])
 def topup_meter():
-    mid = request.args.get('mid')
+    if request.method == 'GET':
+        mid = request.args.get('mid')
+    elif request.method == 'POST':
+        try:
+            json_data = request.get_json()
+            if json_data:
+                mid = json_data.get('mid')
+            else:
+                return make_response('', 422)
+        except Exception:
+            return make_response('', 422)
     if not Registry.query.get(mid):
         return make_response('', 403)
     else:
